3b9e614c11a9cbac602c526475600efef4b97f14
Aifrion <brianyee@Brians-MacBook-Pro.local>
2021-08-19 12:10:45 -0700

    added working navbar and tables for dashboards

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home', views.home),
     path('', views.index),
     path('start', views.start),
+    path('logout', views.logout),
     path('sign_in', views.sign_in),
     path('login', views.login),
     path('create', views.create),
     path('register', views.register),
-    path('check_admin', views.check_admin)
+    path('check_admin', views.check_admin),
+    path('dashboard', views.dashboard),
+    path('dashboard/admin', views.admin_dashboard)
 ]
--- user/views.py
+++ user/views.py
@@ -3,11 +3,13 @@ from django.contrib import messages
 import bcrypt
 from .models import User
 # Create your views here.
+def home(request):
+    return redirect('/')
 def index(request):
     return render(request, 'index.html')
 
 def start(request):
-    return redirect('/sign_in')
+    return redirect('/login')
 
 def sign_in(request):
     try:
@@ -17,12 +19,27 @@ def sign_in(request):
         return redirect('/login')
     if bcrypt.checkpw(request.POST['password'].encode(), account.password.encode()):
         request.session['user_id'] = account.id
+        request.session['user_level'] = account.user_level
         request.session['first_name'] = account.first_name
         request.session['last_name'] = account.last_name
         request.session['email'] = account.email
         return redirect('/check_admin')
     messages.error(request, "Email or password is incorrect")
     return redirect('/login')
+
+def logout(request):
+    if 'user_id' in request.session:
+        del request.session['user_id']
+    if 'user_level' in request.session:
+        del request.session['user_level']
+    if 'first_name' in request.session:
+        del request.session['first_name']
+    if 'last_name' in request.session:
+        del request.session['last_name']
+    if 'email' in request.session:
+        del request.session['email']
+    return redirect('/login')
+
 def login(request):
     return render(request, 'login.html')
 
@@ -44,6 +61,7 @@ def create(request):
         if user.id == 1:
             user.user_level = 9
             user.save()
+        request.session['user_level'] = user.user_level
         request.session['user_id'] = user.id
         return redirect('/check_admin')
 
@@ -51,7 +69,23 @@ def register(request):
     return render(request, 'register.html')
 
 def check_admin(request):
+    level = request.session['user_level']
+    if level == 9:
+        return redirect('/dashboard/admin')
+    else:
+        return redirect('/dashboard')
+
+def admin_dashboard(request):
     context = {
-        'user': User.objects.get(id = request.session['user_id'])
+        'user': User.objects.get(id = request.session['user_id']),
+        'users': User.objects.all()
     }
-    return render(request, 'admin.html', context)
+    return render(request, 'admin_dash.html', context)
+
+def dashboard(request):
+    context = {
+        'user': User.objects.get(id = request.session['user_id']),
+        'users': User.objects.all()
+    }
+    return render(request, 'dash.html', context)
+
